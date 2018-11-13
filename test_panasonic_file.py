8639ce46537e309191dfe985e6458ecae3643f7a
kanhua <kanhua.lee@gmail.com>
2018-11-13 23:51:14 +0900

    v3 version works for 3J

--- test_panasonic_file.py
+++ test_panasonic_file.py
@@ -6,13 +6,16 @@ from solcore.structure import Junction
 
 from solcore.solar_cell import SolarCell
 from solcore.light_source import LightSource
-from dynamic_pixel import solve_quasi_3D
+from spice.dynamic_pixel import solve_quasi_3D
 
-
-illuminationMask = imread('/Users/kanhua/Dropbox/Programming/solar-cell-circuit/private_data/Illumination_profile_20181016.png')
+illuminationMask = imread(
+    '/Users/kanhua/Dropbox/Programming/solar-cell-circuit/private_data/Illumination_profile_20181016.png')
 contactsMask = imread('/Users/kanhua/Dropbox/Programming/solar-cell-circuit/private_data/Mask_profile_20181016.png')
 
-assert illuminationMask.shape==contactsMask.shape
+illuminationMask = illuminationMask[:, :, 0]
+contactsMask = contactsMask[:, :, 0]
+
+assert illuminationMask.shape == contactsMask.shape
 
 print(np.max(illuminationMask))
 print(np.max(contactsMask))
@@ -23,8 +26,11 @@ nx, ny = illuminationMask.shape
 # multiply the current by 4
 center_x = int(nx / 2)
 center_y = int(ny / 2)
-illuminationMask = illuminationMask[center_x:center_x + 50, center_y:center_y + 50]
-contactsMask = contactsMask[center_x:center_x + 50, center_y:center_y + 50]
+
+dx = 200
+dy = 200
+illuminationMask = illuminationMask[center_x:center_x + dx, center_y:center_y + dy]
+contactsMask = contactsMask[center_x:center_x + dx, center_y:center_y + dy]
 
 # Size of the pixels (m)
 Lx = 10e-3
@@ -41,7 +47,7 @@ Rline = 2e-8
 
 # Bias (V)
 vini = 0
-vfin = 3.0
+vfin = 2.0
 step = 0.01
 
 T = 298
@@ -55,14 +61,14 @@ light_source = LightSource(source_type='standard', version='AM1.5g', x=wl, outpu
 
 options = {'light_iv': True, 'wavelength': wl, 'light_source': light_source}
 
-test_pixel_width = [1, 2, 3, 4]
+test_pixel_width = [5]
 
 for pw in test_pixel_width:
     db_junction3 = Junction(kind='2D', T=T, reff=0.5, jref=300, Eg=1.8, A=1, R_sheet_top=100,
                             R_sheet_bot=100,
                             R_shunt=1e16, n=3.5)
     my_solar_cell = SolarCell([db_junction3], T=T)
-    V, I, Vall, Vmet = solve_quasi_3D(my_solar_cell, illuminationMask, contactsMask, options=options, Lx=Lx,
+    V, I = solve_quasi_3D(my_solar_cell, illuminationMask, contactsMask, options=options, Lx=Lx,
                                       Ly=Ly,
                                       h=h,
                                       R_back=1e-16, R_contact=Rcontact, R_line=Rline, bias_start=vini,
@@ -71,5 +77,5 @@ for pw in test_pixel_width:
 
     plt.plot(V, I, label="pw: {}".format(pw))
 
-plt.ylim([-0.03, 0])
+plt.ylim([np.min(I) * 1.1, 0])
 plt.show()
