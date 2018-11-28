bug: fix the normalization problem of jsc

spice/dynamic_pixel.py
@@ -129,7 +129,7 @@ def resize_illumination(illumination, contact_mask, coord_set: np.array, thresho
                         coord_set[r_index, c_index, 2]:coord_set[r_index, c_index, 3]]
 
             #TODO should be np.sum()
-            resized_illumination[r_index, c_index] = np.mean(sub_image)
+            resized_illumination[r_index, c_index] = np.sum(sub_image)
 
     return resized_illumination
 

spice/pixel_processor.py
@@ -28,16 +28,17 @@ def _load_solarcell_param(solarcell: SolarCell, param_name) -> np.array:
 
 class PixelProcessor(object):
 
-    def __init__(self, solarcell: SQCell, lx, ly, h):
+    def __init__(self, solarcell: SQCell, lx, ly, h, gn=1):
         self.solarcell = solarcell
         self._cicuit_params = dict()
         self.lx = lx
         self.ly = ly
-        self.finger_h=h
+        self.finger_h = h
+        self.gn = gn
         self._set_circuit_params()
 
     def _set_circuit_params(self):
-        raw_isc = _load_solarcell_param(self.solarcell, 'jsc')
+        self.raw_isc = _load_solarcell_param(self.solarcell, 'jsc')
         self.raw_i01 = _load_solarcell_param(self.solarcell, 'j01')
         self.raw_i02 = _load_solarcell_param(self.solarcell, 'j02')
         raw_rs_top = _load_solarcell_param(self.solarcell, 'rs_top')
@@ -54,16 +55,11 @@ class PixelProcessor(object):
         self.n1 = _load_solarcell_param(self.solarcell, 'n1')
         self.n2 = _load_solarcell_param(self.solarcell, 'n2')
 
-        # TODO use dummy isc: because isc is not universal in every pixel
-        #self.gn = np.sqrt(1.0 / 340)
-        # TODO temporarily disable gn
-        self.gn = 1
-
         self.area_per_pixel = self.lx * self.ly
 
         # TODO There are problems in this normalization
 
-        self._cicuit_params['isc'] = raw_isc * self.area_per_pixel * self.gn
+        self._cicuit_params['isc'] = self.raw_isc * self.area_per_pixel * self.gn
         # self._cicuit_params['i01'] = raw_i01 * area_per_pixel * gn
         # self._cicuit_params['i02'] = raw_i02 * area_per_pixel * gn
         self._cicuit_params['rs_top'] = raw_rs_top / self.gn
@@ -75,24 +71,21 @@ class PixelProcessor(object):
         # self._cicuit_params['x_metal_top'] = raw_r_line / gn
         # self._cicuit_params['y_metal_top'] = raw_r_line / gn
 
-
-        self.r_line = self.rho_metal/ self.rho_metal / self.gn
-
+        self.r_line = self.rho_metal / self.rho_metal / self.gn
 
         # TODO threshold of metal value
         self.metal_threshold = 0
 
-    def header_string(self,pw):
+    def header_string(self, pw):
 
-        #TODO patch pw:
+        # TODO patch pw:
 
-        self.i01 = self.raw_i01 * self.area_per_pixel*pw*pw * self.gn
-        self.i02 = self.raw_i02 * self.area_per_pixel *pw*pw* self.gn
-
+        self.i01 = self.raw_i01 * self.area_per_pixel * pw * pw * self.gn
+        self.i02 = self.raw_i02 * self.area_per_pixel * pw * pw * self.gn
 
         return create_header(I01=self.i01, I02=self.i02, n1=self.n1, n2=self.n2, Eg=self.eg)
 
-    def node_string(self,  idx, idy, sub_image, lx, ly, is_boundary_x=False,is_boundary_y=False):
+    def node_string(self, idx, idy, sub_image, lx, ly, is_boundary_x=False, is_boundary_y=False):
 
         assert sub_image.size > 0
 
@@ -104,17 +97,19 @@ class PixelProcessor(object):
 
         merged_pixel_area = merged_pixel_lx * merged_pixel_ly
 
+        self._cicuit_params['isc'] = self.raw_isc * lx*ly * self.gn
+
         if metal_coverage > self.metal_threshold:
             agg_contact = self.r_contact / (merged_pixel_area * metal_coverage) / self.gn
-            type='Bus'
+            type = 'Bus'
         else:
             agg_contact = np.inf
-            type='Normal'
+            type = 'Normal'
 
         return create_node(type, idx=idx, idy=idy, Lx=self.lx, Ly=self.ly,
                            x_metal_top=merged_pixel_lx,
                            y_metal_top=merged_pixel_ly, r_contact=agg_contact,
-                           boundary_x=is_boundary_x,boundary_y=is_boundary_y,
+                           boundary_x=is_boundary_x, boundary_y=is_boundary_y,
                            **self._cicuit_params)
 
 

spice/spice_solver.py
@@ -96,6 +96,9 @@ class SPICESolver(object):
         self.v_junc = None
         self.steps = int(np.floor((self.v_end - self.v_start) / self.v_steps) + 1)
 
+        # TODO temporarily add gn here
+        self.gn = 1e7
+
         self.spice_preprocessor = spice_preprocessor
 
         header = self._generate_header()
@@ -109,12 +112,14 @@ class SPICESolver(object):
 
         self._parse_output()
 
+        self._renormalize_output()
+
     def _generate_header(self):
 
         # TODO: this is a patch. It should be a representative pixel in illumination profile
         self.solarcell.set_input_spectrum(load_astm("AM1.5g"))
 
-        px = PixelProcessor(self.solarcell, lx=self.lx, ly=self.ly, h=self.finger_h)
+        px = PixelProcessor(self.solarcell, lx=self.lx, ly=self.ly, h=self.finger_h, gn=self.gn)
 
         return px.header_string(pw=self.rw)
 
@@ -145,9 +150,11 @@ class SPICESolver(object):
                 sub_image = self.metal_contact[coord_set[r_index, c_index, 0]:coord_set[r_index, c_index, 1],
                             coord_set[r_index, c_index, 2]:coord_set[r_index, c_index, 3]]
 
+                # set concentration
                 self.solarcell.set_input_spectrum(illumination_value * self.spectrum)
 
-                px = PixelProcessor(self.solarcell, self.lx, self.ly, h=self.finger_h)
+                px = PixelProcessor(self.solarcell, self.lx, self.ly, h=self.finger_h, gn=self.gn)
+
                 spice_body += px.node_string(c_index, r_index,
                                              sub_image=sub_image, lx=self.lx, ly=self.ly)
 
@@ -186,6 +193,11 @@ class SPICESolver(object):
                     V_junc[yy, xx, :] = 0
 
         self.v_junc = V_junc
+
+    def _renormalize_output(self):
+
+        # self.v_junc=self.v_junc*gn
+        self.I = self.I / self.gn
 
     def get_end_voltage_map(self):
 

spice/tests/test_full_dynamic_pixel_v2.py
@@ -28,9 +28,12 @@ class FullSimulationWithDynamicPixel(unittest.TestCase):
 
         self.T = 298
 
-        self.default_illuminationMask = imread(os.path.join(file_path, 'masks_illumination.png'))
+        # TODO simplify the illumination mask to np.ones
+        # self.default_illuminationMask = imread(os.path.join(file_path, 'masks_illumination.png'))
 
         self.default_contactsMask = imread(os.path.join(file_path, 'masks_sq_no_shades.png'))
+
+        self.default_illuminationMask = np.ones_like(self.default_contactsMask)
 
         # Size of the pixels (m)
         self.Lx = 10e-6
@@ -47,7 +50,7 @@ class FullSimulationWithDynamicPixel(unittest.TestCase):
 
         # Bias (V)
         self.vini = 0
-        self.vfin = 1.6
+        self.vfin = 1.3
         self.step = 0.05
 
         self.pvcell_1j = SQCell(1.42, 300, 1)
@@ -187,6 +190,10 @@ class FullSimulationWithDynamicPixel(unittest.TestCase):
         self.draw_merged_contact_images(test_pixel_width, file_prefix, contacts_mask)
 
         result_vi = None
+
+        print("original image shape:{},{}".format(*contacts_mask.shape))
+        print("Jsc: {:2f} A/m^2".format(self.pvcell_1j.jsc))
+        print("illumination total {}:".format(illumination_mask.sum()))
 
         plt.figure()
 