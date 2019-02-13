87c2a7f5406a62f60b33ad2988306d8de49c6aee
kanhua <kanhua.lee@gmail.com>
2019-02-13 15:06:45 +0900

    test: completed the wrapping of the test cases

--- run_demo_suites.sh
+++ run_demo_suites.sh
@@ -5,4 +5,9 @@ python setup_spice.py ngspice /results
 
 echo "running test cases"
 cd tests
-python -m unittest test_vary_pw_cases.PaperFigure.test_triang_3j
+python -m unittest test_vary_pw_cases.PaperFigure
+
+cd ../diagnostics
+python plot_yaml_data.py highres_triang_1x_dataset.yaml
+python plot_yaml_data.py highres_triang_500x_dataset.yaml
+python plot_yaml_data.py highres_triang_500x_10mm_dataset.yaml
