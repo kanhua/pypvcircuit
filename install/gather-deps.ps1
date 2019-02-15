07c563c7a4e6d1de006d9c52de34f1de049df693
kanhua <kanhua.lee@gmail.com>
2019-02-15 18:10:44 +0900

    fea: add script that gathers the python dependencies for installing offline.

---
+++ install/gather-deps.ps1
@@ -0,0 +1,7 @@
+﻿Invoke-Expression (Join-Path $PSScriptRoot load-condaenv.ps1)
+
+# this script prepares software for offline install
+$pip_command="pip download --dest .\deps pint msgpack"
+
+Invoke-Expression $pip_command
+Compress-Archive -Path .\deps -DestinationPath deps.zip
