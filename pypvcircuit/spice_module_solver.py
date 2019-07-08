60a43b3ec113ee340610356b25734a460aac662e
kanhua <kanhua.lee@gmail.com>
2019-07-08 21:36:56 +0900

    fea: draft the module solver

---
+++ pypvcircuit/spice_module_solver.py
@@ -0,0 +1,158 @@
+import typing
+import math
+import numpy as np
+
+from pypvcell.solarcell import SolarCell
+from pypvcell.illumination import load_astm
+from pypvcell.spectrum import Spectrum
+
+from .parse_spice_output import parse_output
+from .pixel_processor import PixelProcessor, create_header
+from .spice_interface import solve_circuit
+
+
+class SingleModuleStringSolver(object):
+
+    def __init__(self, solarcell: SolarCell, illumination: float, v_start,
+                 v_end, v_steps, l_r, l_c, cell_number, spice_preprocessor=None):
+        self.solarcell = solarcell
+
+        self.l_r = l_r
+        self.l_c = l_c
+        self.illumination = illumination
+
+        self.v_start = v_start
+        self.v_end = v_end
+        self.v_steps = v_steps
+
+        self.cell_number = cell_number
+
+        self.V = None
+        self.I = None
+
+        self.spice_preprocessor = spice_preprocessor
+
+        self.gn = self._find_gn()
+
+        # self._solve_circuit()
+
+    def _solve_circuit(self):
+        # TODO add temperature as an object parameter
+        header = self._generate_header(temperature=20)
+        nodes = self._generate_network()
+        exec = self._generate_exec()
+        spice_footer = ".end"
+        self.spice_input = header + nodes + exec + spice_footer
+        self.raw_results = self._send_command()
+        self._parse_output()
+        self._renormalize_output()
+
+    def _send_command(self):
+
+        raw_results = solve_circuit(spice_file_contents=self.spice_input,
+                                    postprocess_input=None)
+
+        return raw_results
+
+    def _generate_header(self, temperature):
+
+        title = "*** A SPICE simulation with python\n\n"
+
+        options = ".OPTIONS TNOM=20 TEMP={0}\n\n".format(temperature)
+
+        header_string = title + options
+
+        return header_string
+
+    def _find_gn(self):
+        sample_isc = 340
+
+        isc = self.illumination * sample_isc * self.l_c * self.l_r
+
+        return 1 / isc * 100
+
+    def _generate_network(self):
+
+        self.solarcell.set_input_spectrum(load_astm("AM1.5g") * self.illumination)
+
+        spj = ""
+        node_count = 0
+        junction_count = 0
+        for cn in range(self.cell_number):
+
+            lowerBypassConnection = node_count
+            for jn in range(len(self.solarcell.subcell)):
+                spj += spice_junction(junction_count, node_count,
+                                      self.solarcell.subcell[jn].jsc * self.l_c * self.l_r * self.gn,
+                                      self.solarcell.subcell[jn].j01 * self.l_c * self.l_r * self.gn,
+                                      self.solarcell.subcell[jn].j02,
+                                      n1=1, n2=2, Eg=self.solarcell.subcell[jn].eg, rsh=1e14)
+                junction_count += 1
+                node_count += 1
+
+                # Add the series resistance
+            # spiceout = 'r{0} {1} {2} {3}\n'.format(2 * junction_count - 1, node_count+1, node_count, 1e-8)
+            # node_count += 1
+            # spj+=spiceout
+
+            # Connect bypass diode.  Connections are: lowerBypassConnection & nodeCounter
+            # spiceout = 'd{0} {1} {2} bypassdiode\n'.format(2 * junction_count + 1, lowerBypassConnection, node_count)
+            # spj+=spiceout
+            # junction_count += 1
+
+        # add bias
+
+        spj += "vdep " + str(junction_count) + " 0\n"
+
+        return spj
+
+    def _parse_output(self):
+        results = parse_output(self.raw_results)
+
+        self.V, self.I = results['dep#branch']
+
+    def _renormalize_output(self):
+
+        # self.v_junc=self.v_junc*gn
+        self.I = -self.I / self.gn
+
+    def _generate_exec(self):
+
+        # We prepare the SPICE execution
+        return ".PRINT DC i(vdep)\n.DC vdep {0} {1} {2}\n".format(self.v_start, self.v_end, self.v_steps)
+
+
+def spice_junction(jc, nc, isc, j01, j02, n1, n2, Eg, rsh):
+    """ Creates the string representation in SPICE of the junciton defined by the input values.
+
+    This function was adopted from solcore5:
+    solcore.spice.pv_module_solver.spice_junction
+
+    :param jc: junction counter
+    :param nc: node counter
+    :param isc: Short circuit current
+    :param j01: Reverse saturation current corresponding to ideality factor n1
+    :param j02: Reverse saturation current corresponding to ideality factor n2
+    :param n1: Ideality factor, typically = 1
+    :param n2: Ideality factor, typically = 2
+    :param Eg: Bandgap of the material the junction is made off
+    :param rsh: Shunt resistance in the junction
+    :return: A string representation of the junciton in SPICE
+    """
+
+    isource = 'i{0} {1} {2} dc {3}\n'.format(jc, nc, nc + 1, isc)
+    d1 = 'd{0} {1} {2} diode{3} OFF\n'.format(2 * jc - 1, nc + 1, nc, 2 * jc - 1)
+    d1deff = '.model diode{0} d(is={1},n={2},eg={3})\n'.format(2 * jc - 1, j01, n1, Eg)
+
+    if j02 is not None:
+        d2 = 'd{0} {1} {2} diode{3} OFF\n'.format(2 * jc, nc + 1, nc, 2 * jc)
+        d2deff = '.model diode{0} d(is={1},n={2},eg={3})\n'.format(2 * jc, j02, n2, Eg)
+    else:
+        d2 = ""
+        d2deff = ""
+
+    rshunt = 'r{0} {1} {2} {3}\n'.format(2 * jc, nc + 1, nc, rsh)
+    # rshunt=""
+    junction = isource + d1 + d1deff + d2 + d2deff + rshunt
+
+    return junction
---
+++ tests/test_module_solver.py
@@ -0,0 +1,58 @@
+import unittest
+from pypvcell.solarcell import SQCell, MJCell
+
+from pypvcircuit.spice_module_solver import SingleModuleStringSolver
+from pypvcell.illumination import load_astm
+import pypvcell.fom as fom
+
+import matplotlib.pyplot as plt
+
+
+class MyTestCase(unittest.TestCase):
+    def test_module_string_netlist(self):
+        self.gaas_1j = SQCell(1.42, 300, 1)
+        self.ingap_1j = SQCell(1.87, 300, 1)
+        self.ge_1j = SQCell(0.7, 300, 1)
+
+        mj_cell = MJCell([self.ingap_1j, self.gaas_1j, self.ge_1j])
+
+        sm = SingleModuleStringSolver(solarcell=mj_cell, illumination=1,
+                                      v_start=0, v_end=5, v_steps=0.01, l_r=1e-3, l_c=1e-3,
+                                      cell_number=2, spice_preprocessor=None)
+
+        # print(sm._generate_network())
+
+        sm._solve_circuit()
+
+        plt.plot(sm.V, sm.I)
+
+        print(fom.voc(sm.V, sm.I))
+        print(fom.isc(sm.V, sm.I))
+
+        plt.show()
+
+    def test_1j_module_string(self):
+        self.gaas_1j = SQCell(1.42, 300, 1)
+        self.ingap_1j = SQCell(1.87, 300, 1)
+        self.ge_1j = SQCell(0.7, 300, 1)
+
+        mj_cell = MJCell([self.ingap_1j])
+
+        sm = SingleModuleStringSolver(solarcell=mj_cell, illumination=1,
+                                      v_start=0, v_end=10, v_steps=0.01, l_r=10, l_c=10,
+                                      cell_number=4, spice_preprocessor=None)
+
+        # print(sm._generate_network())
+
+        sm._solve_circuit()
+
+        plt.plot(sm.V, sm.I)
+
+        print(fom.voc(sm.V, sm.I))
+        print(fom.isc(sm.V, sm.I))
+
+        plt.show()
+
+
+if __name__ == '__main__':
+    unittest.main()
